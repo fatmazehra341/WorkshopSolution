f53322fe54a387b21434cfc4b99d310ae90838cb
fatmazehra341 <fatmazehra341@gmail.com>
2022-05-28 11:54:06 +0530

    [Fatma]: checking given year is leap year or not

---
+++ WorkshopProblemStatement/LeapYears.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WorkshopProblemStatement
+{
+    public class LeapYears
+    {
+        public void leapYears()
+        {
+            Console.WriteLine("Enter a year for checking a leap year or not");
+            int year = Convert.ToInt32(Console.ReadLine());
+            if ((year % 4 == 0 || year % 100! == 0) || year % 400 == 0)
+            {
+                Console.WriteLine("Given year is a leap year ");
+            }
+            else
+            {
+                Console.WriteLine("Given year is not a leap year");
+            }
+        }
+    }
+}
---
+++ WorkshopProblemStatement/Program.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using WorkshopProblemStatement;
+public class Program
+{
+    public static void Main(String[] args)
+    {
+        LeapYears leapYearProg = new LeapYears();
+        leapYearProg.leapYears();
+    }
+}
